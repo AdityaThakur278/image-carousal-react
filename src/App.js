741b8c842faeebda24d59250175af1f0d76575b2
Aditya Thakur <takumarsingh@tekion.com>
2023-12-10 19:29:27 +0530

    task complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from "react";
+import { IMAGES } from "./mockData";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const handleLeftArrowClick = useCallback(() => {
+    setSelectedImageIndex(
+      (selectedImageIndex - 1 + IMAGES.length) % IMAGES.length,
+    );
+  }, [selectedImageIndex]);
+
+  const handleRightArrowClick = useCallback(() => {
+    setSelectedImageIndex((selectedImageIndex + 1) % IMAGES.length);
+  }, [selectedImageIndex]);
+
+  const handleImageSelect = useCallback((imageIndex) => {
+    setSelectedImageIndex(imageIndex);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent-container">
+      <div className="row-container">
+        <img
+          src={IMAGES[selectedImageIndex].image_url}
+          className="selected-image"
+          alt="selected"
+        />
+        <div onClick={handleLeftArrowClick} className="arrow-left-container">
+          &lt;
+        </div>
+        <div onClick={handleRightArrowClick} className="arrow-right-container">
+          &gt;
+        </div>
+      </div>
+      <div className="image-options-parent-container">
+        {IMAGES.map((image, imageIndex) => (
+          <div
+            key={image.caption}
+            className="image-options-container"
+            style={{ width: `${100 / IMAGES.length}%` }}
+            onClick={() => handleImageSelect(imageIndex)}
+          >
+            <img
+              src={image.image_url}
+              className={
+                imageIndex === selectedImageIndex
+                  ? "image-options selected-image-border"
+                  : "image-options"
+              }
+              alt="options"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
